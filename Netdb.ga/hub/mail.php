7094fd913706260cf1eba5f5616bb2cf2bcf4c98
Yannick Füreder <64203572+YannickFuereder@users.noreply.github.com>
2021-08-11 08:55:44 +0200

    Delete Netdb.ga/hub directory

--- Netdb.ga/hub/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$from = "support@netdb.ga";
-$to = $_GET["to"];
-$subject = $_GET["subject"];
-$message = $_GET["message"];
-$headers = "From:" . $from;
-mail($to,$subject,$message, $headers);
-echo "Test email sent";
-?>
